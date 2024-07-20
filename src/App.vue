1a5a9eba21d8e6d91a6b90bc5e9a048d8e583c3c
GitHub name9807 <GitHub 3416431988@qq.com>
2024-07-20 18:30:28 +0800

    先弄过去

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,28 @@
 <template>
+    <enter-game></enter-game>
+
 </template>
 
 <script>
+    import WhiteScreen from '../components/WhiteScreen.vue';
+import EnterGame from '../views/EnterGame.vue';
+
+    export default{
+        components:{
+            WhiteScreen,
+            EnterGame
+        }
+    }
 </script>
 
-<style></style>
+<style>
+.d {
+    width: 100vw;
+    height: 100vh;
+    background-color: green;
+}
+ * {
+    padding: 0;
+    margin: 0;
+ }
+</style>
---
+++ views/EnterGame.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="main">
+    <WhiteScreen></WhiteScreen>
+  </div>
+</template>
+
+<script>
+import WhiteScreen from '../components/WhiteScreen.vue'
+
+export default {
+    components:{
+        WhiteScreen
+    }
+
+}
+</script>
+
+<style>
+    .main {
+        width: 100vw;
+        height: 100vh;
+
+
+    }
+    /* 根据视口宽度判断设备 */
+@media (max-width: 768px) {
+  /* 手机设备的样式 */
+  .main{
+    background-color: red;
+    opacity: 0.7;
+        background: url(../static/1.png);
+        background-size: contain;
+        background-repeat: no-repeat;
+  }
+}
+
+@media (min-width: 769px) {
+  /* 电脑设备的样式 */
+  .main{
+    background-color: red;
+    opacity: 0.7;
+        background: url(../static/output.jpg);
+        background-size: contain;
+        background-repeat: no-repeat;
+        background-position:  center center;
+  }
+}
+
+</style>
